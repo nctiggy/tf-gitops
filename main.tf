30a278caa758edaf2babd4d372beea1bc9adcab0
Craig Smith <nctiggy@gmail.com>
2021-08-13 14:30:06 -0700

    adding vars

--- main.tf
+++ main.tf
@@ -10,6 +10,25 @@ resource "google_container_cluster" "test" {
   enable_autopilot = true
 }
 
+data "google_client_config" "default" {
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host  = "https://${google_container_cluster.test.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
+
+data "kubernetes_all_namespaces" "allns" {}
+
+output "all-ns" {
+  value = data.kubernetes_all_namespaces.allns.namespaces
+}
+
 output "endpoint" {
   value = google_container_cluster.test.endpoint
 }
