ccc83eec5c0e5cafecf1daa7266f945126ce9107
Craig Smith <nctiggy@gmail.com>
2021-08-13 17:59:46 -0700

    deploy contour

--- main.tf
+++ main.tf
@@ -13,12 +13,26 @@ resource "google_container_cluster" "test" {
 data "google_client_config" "default" {
 }
 
-provider "kubernetes" {
-  host  = "https://${google_container_cluster.test.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    google_container_cluster.test.master_auth[0].cluster_ca_certificate,
-  )
+provider "helm" {
+  kubernetes {
+    host  = "https://${google_container_cluster.test.endpoint}"
+    token = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(
+      google_container_cluster.test.master_auth[0].cluster_ca_certificate,
+    )
+  }
+}
+
+resource "helm_release" "contour" {
+  name       = "contour"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "contour"
+  version    = "1.18.0"
+
+  set {
+    name  = "envoy.useHostPort"
+    value = "false"
+  }
 }
 
 output "endpoint" {
